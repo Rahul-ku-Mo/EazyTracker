165abfc27195506c1c28ef2a295e9de14538efad
Rahul Ku Mohanty <rajamohanty12345@gmail.com>
2025-03-12 16:55:57 +0530

    fix: fixed typo related to context files

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, useState } from "react";
+
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+
+interface AuthContextType {
+  isLoggedIn: boolean;
+  setIsLoggedIn: (isLoggedIn: boolean) => void;
+  accessToken: string;
+}
+
+const AuthContext = createContext<AuthContextType>({} as AuthContextType);
+
+const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate();
+  const accessToken = Cookies.get("accessToken") || "";
+  const [isLoggedIn, setIsLoggedIn] = useState(!!accessToken);
+
+  /**1. verify user logged in is Authorized or not */
+  useQuery({
+    queryKey: ["auth-verify"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get<{ success: boolean; role: string }>(
+          `${import.meta.env.VITE_API_URL}/auth/verify`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        return response.data;
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          const status = error.response?.status;
+          if (status === 401 || status === 403) {
+            Cookies.remove("accessToken");
+            setIsLoggedIn(false);
+            navigate("/auth");
+          }
+        }
+        throw error;
+      }
+    },
+    enabled: !!accessToken,
+    retry: false,
+  });
+
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        accessToken
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContextProvider, AuthContext };
